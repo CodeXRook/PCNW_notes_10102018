66b1dc656dd972acdd48617d37da1ea6333eea1a
Mottaqui Karim <taqqui.karim@gmail.com>
2018-10-10 19:52:27 -0400

    added a few ways to write functions

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-const fifthPow = (num) => {
+// function declaration
+function fifthPow$(num) {
     return num ** 5;
 }
+
+// function expression - anon
+const fifthPow$$ = function(num) {
+
+};
+
+// NAMED function expression
+const fifthPow$$$ = function someRandomNamelol(num) {
+
+}
+
+// function expression - anon 
+const fifthPow = (num) => {
+    return num ** 5;
+};
+
+const fifthPow$$$$ = num => num ** 5;
+const nthPow = (num, n) => num ** n;
+/*
+function nthPow(num, n) {
+    return num ** n;
+}
+*/
 
 console.log(fifthPow(2)) // 32
