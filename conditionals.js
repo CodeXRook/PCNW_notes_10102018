f3505e69ffde7609714c0c7aede2999041d12b8b
Mottaqui Karim <taqqui.karim@gmail.com>
2018-10-10 20:46:44 -0400

    conditionals and pset

---
+++ conditionals.js
@@ -0,0 +1,43 @@
+const simpleConditionalExample = function() {
+    if (true) {
+        // code in between the curly brackets will ONLY run
+        // if the expression inside the (...) is true
+        console.log('this will show up!');
+    }
+
+    if (false) {
+        console.log('will this show up tho...?')
+    }
+
+    return 'this is a test';
+}
+
+simpleConditionalExample();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
